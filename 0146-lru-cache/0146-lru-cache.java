a559c1c5238b8e701ab3962873896b7d14bb9e4c
Anchal Maurya <147910912+anchalmaurya29@users.noreply.github.com>
2026-09-21 11:55:52 +0530

    Time: 15 ms (72.78%), Space: 58.3 MB (90.26%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,48 @@
+class LRUCache {
+    class Node {
+        int key, value;
+        Node prev, next;
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    int capacity;
+    HashMap<Integer, Node> map = new HashMap<>();
+    Node head = new Node(0, 0);
+    Node tail = new Node(0, 0);
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        remove(node);
+        add(node);
+        return node.value;
+    }
+    public void put(int key, int value) {
+        if (map.containsKey(key))
+            remove(map.get(key));
+        Node node = new Node(key, value);
+        map.put(key, node);
+        add(node);
+        if (map.size() > capacity) {
+            Node lru = head.next;
+            remove(lru);
+            map.remove(lru.key);
+        }
+    }
+    void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    void add(Node node) {
+        node.prev = tail.prev;
+        node.next = tail;
+        tail.prev.next = node;
+        tail.prev = node;
+    }
+}
